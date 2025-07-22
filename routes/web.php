6cc32a3068cd9459ce15a8b48d7332a98b845631
lafathalfath <iqnaraidan12@gmail.com>
2025-07-22 16:51:03 +0700

    `Removed unused button and API route, updated routes/web.php`

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\AppSettingController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LandingPageController;
-use App\Http\Middleware\AuthenticatedMiddleware;
-use App\Http\Middleware\GuestMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,10 +19,3 @@ Route::middleware(['authenticated', 'prevent-back-history'])->group(function () 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/manage/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
-
-// api
-// Route::middleware('api')
-// ->prefix('api')
-// ->group(function () {
-//     Route::get('/tes', [AppSettingController::class, 'tes'])->middleware('auth:sanctum');
-// });
