f27ed0e3e10d97ce1be1a173d1bf63bdf49d47dc
Farchan <farkun>
2025-07-30 14:27:42 +0700

    documentsection

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AppSetting;
 use App\Models\CarouselImage;
 use App\Models\Hero;
+use App\Models\Document;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
@@ -12,10 +13,12 @@ class LandingPageController extends Controller
     public function index() {
         $app_setting = AppSetting::first();
         $hero = Hero::first();
+        $documents = Document::orderBy('id')->get();
         $carousel_image = CarouselImage::get();
         return view('landingPage', [
             'app_setting' => $app_setting,
             'hero' => $hero,
+            'documents' => $documents,
             'carousel_image' => $carousel_image,
         ]);
     }
